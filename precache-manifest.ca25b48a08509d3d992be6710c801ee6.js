e2ff9f34b788c0307750ac1434b66257d69b0166
Nathan Kanigsberg <nathan@kanigsberg.com>
2020-09-27 16:45:33 -0400

    Updates

---
+++ precache-manifest.ca25b48a08509d3d992be6710c801ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686c91807f9782e056edc7444b1e16d5",
+    "url": "/nathanKanigsbergProject5/index.html"
+  },
+  {
+    "revision": "e2b4e8d534964ba81e73",
+    "url": "/nathanKanigsbergProject5/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "761bc264329833f1d9b6",
+    "url": "/nathanKanigsbergProject5/static/js/2.88c6c092.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nathanKanigsbergProject5/static/js/2.88c6c092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b4e8d534964ba81e73",
+    "url": "/nathanKanigsbergProject5/static/js/main.e7873be0.chunk.js"
+  },
+  {
+    "revision": "29a2dad004b0c0b60f2d",
+    "url": "/nathanKanigsbergProject5/static/js/runtime-main.ff34a727.js"
+  }
+]);
