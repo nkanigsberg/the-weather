885b17bd96f1cc066b494884342f0ca1e836bfb3
Nathan Kanigsberg <nathan@kanigsberg.com>
2020-09-30 16:44:06 -0400

    Updates

---
+++ precache-manifest.f18cb46593a0ea23181a462f1a33b7a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2109e6dd4f319cf5878ea5a401fea11c",
+    "url": "/nathanKanigsbergProject5/index.html"
+  },
+  {
+    "revision": "18657a8782fb72ae9165",
+    "url": "/nathanKanigsbergProject5/static/css/main.1c3e791f.chunk.css"
+  },
+  {
+    "revision": "89373106207587e1af10",
+    "url": "/nathanKanigsbergProject5/static/js/2.26cf3a45.chunk.js"
+  },
+  {
+    "revision": "25a6ee77f77f2ea23b1eeda77e4de3b5",
+    "url": "/nathanKanigsbergProject5/static/js/2.26cf3a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18657a8782fb72ae9165",
+    "url": "/nathanKanigsbergProject5/static/js/main.dd8b2808.chunk.js"
+  },
+  {
+    "revision": "29a2dad004b0c0b60f2d",
+    "url": "/nathanKanigsbergProject5/static/js/runtime-main.ff34a727.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nathanKanigsbergProject5/precache-manifest.cf4ed9b8e0fc3439adbd1e53d35599f3.js"
+  "/nathanKanigsbergProject5/precache-manifest.f18cb46593a0ea23181a462f1a33b7a7.js"
 );
 
 self.addEventListener('message', (event) => {
