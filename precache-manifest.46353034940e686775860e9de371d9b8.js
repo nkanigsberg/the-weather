4910769cce2bac2baeaf2d8661c0e6e5f50e6fa1
Nathan Kanigsberg <nathan@kanigsberg.com>
2020-10-12 14:40:48 -0400

    Updates

---
+++ precache-manifest.46353034940e686775860e9de371d9b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bc6294118f7fc58f1980144d59a604",
+    "url": "/TheWeather/index.html"
+  },
+  {
+    "revision": "d98f98ae98e96fa227ff",
+    "url": "/TheWeather/static/css/main.0b215a2a.chunk.css"
+  },
+  {
+    "revision": "b8062456af886c59d6ae",
+    "url": "/TheWeather/static/js/2.ca634a7b.chunk.js"
+  },
+  {
+    "revision": "25a6ee77f77f2ea23b1eeda77e4de3b5",
+    "url": "/TheWeather/static/js/2.ca634a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98f98ae98e96fa227ff",
+    "url": "/TheWeather/static/js/main.fab43f30.chunk.js"
+  },
+  {
+    "revision": "de4fe8738cc8357b04fc",
+    "url": "/TheWeather/static/js/runtime-main.cfe0a734.js"
+  }
+]);
